1108f7a4e09a3df9a79846c6cad0c6eb2fce0be3
kkuhn3 <karljkuhn@gmail.com>
2023-10-13 17:18:35 -0400

    grey-out non-check advancements if arch flag is set

--- selectors.js
+++ selectors.js
@@ -10,7 +10,7 @@ function hideAllCategories() {
 	}
 }
 
-function worldUpdate(worldName) {
+function worldUpdate(worldName, isArchipelago) {
 	if (worldName === "") {
 		alert("No valid world selected.");
 	}
@@ -28,14 +28,56 @@ function worldUpdate(worldName) {
 				playerSel.add(option);
 			}
 		}
-		updateNodes(worldName, "all");
+		updateNodes(worldName, "all", isArchipelago);
 	}
 }
 
-function updateNodes(worldName, userId) {
+const archExcludedNodes = [
+	// hard
+	"adventure/very_very_frightening", 
+	"nether/all_potions",
+	"husbandry/bred_all_animals",
+	"adventure/two_birds_one_arrow",
+	"adventure/arbalistic",
+	"adventure/kill_all_mobs",
+	"nether/create_full_beacon",
+	"husbandry/balanced_diet",
+	"nether/uneasy_alliance",
+	"nether/netherite_armor",
+	"husbandry/complete_catalogue",
+	"adventure/lightning_rod_with_villager_no_fire",
+	"adventure/play_jukebox_in_meadows",
+	"adventure/trade_at_world_height",
+	"husbandry/leash_all_frog_variants",
+	"husbandry/froglights",
+	// unreasonable
+	"nether/all_effects",
+	"adventure/adventuring_time",
+	// ender dragon
+	"end/kill_dragon",
+	"end/dragon_egg",
+	"end/respawn_dragon",
+	"end/dragon_breath",
+	"adventure/kill_all_mobs",
+	"adventure/spyglass_at_dragon",
+	// new as of 1.20.1, not yet in archipelago logic
+	"adventure/read_power_of_chiseled_bookshelf",
+	"adventure/trim_with_any_armor_pattern",
+	"adventure/trim_with_all_exclusive_armor_patterns",
+	"adventure/salvage_sherd",
+	"adventure/craft_decorated_pot_using_only_sherds",
+	"husbandry/obtain_sniffer_egg",
+	"husbandry/feed_snifflet",
+	"husbandry/plant_any_sniffer_seed"
+]
+
+function updateNodes(worldName, userId, isArchipelago) {
 	const nodes = document.getElementsByClassName("node");
 	for(node of nodes) {
-		if(data[worldName][userId][node.id].done === 1) {
+		if(isArchipelago && archExcludedNodes.includes(node.id)) {
+			node.style.backgroundColor = "grey";
+		}
+		else if(data[worldName][userId][node.id].done === 1) {
 			node.style.backgroundColor = "green";
 		}
 		else if(data[worldName][userId][node.id].done > 0) {
